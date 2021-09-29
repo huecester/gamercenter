520b5a9075df3bf8a6c1637af5dfb54a26314685
huecester <jericho.paulino@yahoo.com.ph>
2021-09-28 22:23:22 -0700

    Vue todo list

---
+++ public/scripts/index.js
@@ -0,0 +1,31 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			newTodoText: '',
+			todos: [],
+			nextTodoId: 0,
+		};
+	},
+	methods: {
+		addNewTodo() {
+			this.todos.push({
+				id: this.nextTodoId++,
+				text: this.newTodoText,
+			});
+			this.newTodoText = '';
+		},
+	},
+});
+
+app.component('todo-item', {
+	template: `
+		<li>
+			{{ text }}
+			<button @click="$emit('remove')">Remove</button>
+		</li>
+	`,
+	props: ['text'],
+	emits: ['remove'],
+});
+
+app.mount('#todo-list')
