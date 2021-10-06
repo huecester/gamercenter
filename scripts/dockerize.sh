b0886b01079ebb78e8e7322f1765e5ddb963a800
huecester <jericho.paulino@yahoo.com.ph>
2021-10-05 19:16:13 -0700

    Remove linux/arm64 from platforms

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm64/v8 -t huecester/gamercenter --push .
+docker buildx build --platform linux/amd64,linux/arm64/v8 -t huecester/gamercenter --push .
