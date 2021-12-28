01ab4fe2448f7aae10dcf0e488d075dc838a4c1c
huecester <jericho.paulino@yahoo.com.ph>
2021-12-28 06:14:40 -0800

    Add navbar

--- client/src/routes/__layout.svelte
+++ client/src/routes/__layout.svelte
@@ -1,6 +1,13 @@
 <script>
 	import '../app.scss';
 	import logo from '../assets/logo-white-200x150.png';
+
+	const routes = [
+		{ title: 'Home', path: '/' },
+		{ title: 'About', path: '/about' },
+		{ title: 'Games', path: '/games' },
+		{ title: 'Bots', path: '/bots' },
+	];
 </script>
 
 
@@ -16,6 +23,13 @@
 </header>
 
 <nav>
+	<ul class="delist row">
+		{#each routes as route}
+			<li>
+				<a class="delink" href={route.path}>{route.title}</a>
+			</li>
+		{/each}
+	</ul>
 </nav>
 
 <slot />
@@ -27,6 +41,26 @@
 		img {
 			width: 100px;
 			height: auto;
+		}
+	}
+
+	nav ul {
+		margin: 0.5rem 0;
+		background-color: $bg-medium;
+
+		li a {
+			display: block;
+			padding: 0.5rem 1rem;
+			font-style: bold;
+			font-size: 1.1rem;
+			transition: background-color 0.05s;
+
+			&:hover {
+				background-color: $fg-medium;
+			}
+			&:active {
+				background-color: $fg-dark;
+			}
 		}
 	}
 </style>
