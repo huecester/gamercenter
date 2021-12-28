8a5f5b71461e48d466803807e0c5897613083dd6
huecester <jericho.paulino@yahoo.com.ph>
2021-12-28 06:08:15 -0800

    Add header

--- client/src/routes/__layout.svelte
+++ client/src/routes/__layout.svelte
@@ -1,5 +1,32 @@
 <script>
-	import "../app.scss";
+	import '../app.scss';
+	import logo from '../assets/logo-white-200x150.png';
 </script>
 
+
+<header>
+	<a href="/">
+		<img
+			src={logo}
+			alt="gamercenter logo"
+			width="200"
+			height="150"
+		/>
+	</a>
+</header>
+
+<nav>
+</nav>
+
 <slot />
+
+
+<style lang="scss">
+	header {
+		text-align: center;
+		img {
+			width: 100px;
+			height: auto;
+		}
+	}
+</style>
