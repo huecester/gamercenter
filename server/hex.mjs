8b0fa0ca7e55dc0b328b491712f0dece54258464
huecester <jericho.paulino@yahoo.com.ph>
2021-12-28 20:35:31 -0800

    Remove server

--- server/hex.mjs
+++
@@ -1,4 +0,0 @@
-const genHex = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
-
-export const genID = () => genHex(8);
-export const genColor = () => `#${genHex(6)}`
