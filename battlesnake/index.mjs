de6de5f4ed94cc672a736ac1efcef7b44d24c87e
huecester <jericho.paulino@yahoo.com.ph>
2022-01-05 10:58:15 -0800

    Ably testing

--- battlesnake/index.mjs
+++ battlesnake/index.mjs
@@ -1,1 +1,16 @@
-console.log('Hello, world!');
+import dotenv from 'dotenv';
+import Ably from 'ably';
+
+dotenv.config();
+
+const ably = new Ably.Realtime(process.env.ABLY_API_KEY);
+const channel = ably.channels.get('messages');
+
+channel.subscribe('msg', msg => {
+	console.log('Received message:', msg);
+});
+
+ably.connection.on('connected', () => {
+	console.log('Connected to channel.');
+	channel.publish('msg', 'Hello, world!');
+});
