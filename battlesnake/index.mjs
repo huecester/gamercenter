9561251ada9346c598df834844e614e6d8b2f315
huecester <jericho.paulino@yahoo.com.ph>
2022-01-18 18:10:04 -0800

    Remove Ably and Redis client

--- battlesnake/index.mjs
+++ battlesnake/index.mjs
@@ -1,11 +1,4 @@
 import dotenv from 'dotenv';
-import Ably from 'ably';
-import redis from 'redis';
 
 dotenv.config();
 
-const ably = new Ably.Realtime(process.env.ABLY_API_KEY);
-const db = redis.createClient({
-	url: 'rediss://:1214dc8284be4bedbb6cd4087994f6db@eu1-lasting-finch-34286.upstash.io:34286',
-});
-await db.connect();
