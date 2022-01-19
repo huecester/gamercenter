8f41b34493d14de34a0ce98c882c8868350b926c
huecester <jericho.paulino@yahoo.com.ph>
2022-01-18 18:59:17 -0800

    Test testing

---
+++ battlesnake/src/index.spec.js
@@ -0,0 +1,8 @@
+import { describe } from 'mocha';
+import { expect } from 'chai';
+
+describe('Index test suite', () => {
+	it('Should run', () => {
+		expect(1 === 1).to.be.true;
+	});
+});
---
+++ battlesnake/test/test.spec.js
@@ -0,0 +1,8 @@
+import { describe } from 'mocha';
+import { expect } from 'chai';
+
+describe('Test test suite', () => {
+	it('Should evaluate 1 === 1 to true', () => {
+		expect(1).to.equal(1);
+	});
+});
