086146b909dd958878e563424991f423343cb166
huecester <jericho.paulino@yahoo.com.ph>
2022-01-19 11:18:57 -0800

    Remove test specs

--- battlesnake/src/index.spec.js
+++
@@ -1,8 +0,0 @@
-import { describe } from 'mocha';
-import { expect } from 'chai';
-
-describe('Index test suite', () => {
-	it('Should run', () => {
-		expect(1 === 1).to.be.true;
-	});
-});
--- battlesnake/test/test.spec.js
+++
@@ -1,8 +0,0 @@
-import { describe } from 'mocha';
-import { expect } from 'chai';
-
-describe('Test test suite', () => {
-	it('Should evaluate 1 === 1 to true', () => {
-		expect(1).to.equal(1);
-	});
-});
