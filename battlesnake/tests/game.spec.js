48f9ade81ffd908958a3baf83f038e47671a0d69
huecester <jericho.paulino@yahoo.com.ph>
2022-02-02 13:03:29 -0800

    Outline tests

---
+++ battlesnake/tests/game.spec.js
@@ -0,0 +1,53 @@
+import { describe } from 'mocha';
+import chai, { expect } from 'chai';
+import chaiHTTP from 'chai-http';
+import sinon from 'sinon';
+import faker from '@faker-js/faker';
+
+import { clearRooms } from '../src/rooms.js'
+import app from '../src/app.js';
+
+chai.use(chaiHTTP);
+
+
+describe('Game page', () => {
+	let roomname, password, id, username;
+
+	describe('Joining', () => {
+		it('should be able to join a room', () => {
+
+		});
+
+		it('should not be able to join a room that does not exist', () => {
+
+		});
+	});
+
+	describe('Messaging', () => {
+		it('should be able to send a message', () => {
+
+		});
+
+		it('should slice all messages to a max of 64 characters', () => {
+
+		});
+	});
+
+	describe('Leaving', () => {
+		it('should be able to leave', () => {
+
+		});
+
+		it('should disconnect all clients after the host leaves', () => {
+
+		});
+		
+		it('should not disconnect clients when a non-host leaves', () => {
+
+		});
+
+		it('should not be able to join a room that has been closed', () => {
+
+		});
+	});
+});
