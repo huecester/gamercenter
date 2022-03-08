670e60bc8bea0d36f4858a4a6d343cf403e6744e
huecester <jericho.paulino@yahoo.com.ph>
2022-03-07 19:02:47 -0800

    Use mutex for state

--- battlesnake/src/main.rs
+++ battlesnake/src/main.rs
@@ -1,10 +1,13 @@
 #[macro_use] extern crate rocket;
 
+use std::sync::{Arc, Mutex};
 use rocket::{
     serde::json::Json,
     State,
 };
 use battlesnake_server::types::*;
+
+type Rooms = Arc<Mutex<Vec<Room>>>;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -12,18 +15,17 @@ fn index() -> &'static str {
 }
 
 #[get("/rooms")]
-fn rooms(rooms: &State<Vec<Room>>) -> Json<Vec<Room>> {
-    Json(rooms.to_vec())
+fn rooms(rooms: &State<Rooms>) -> Json<Vec<Room>> {
+    Json(Arc::clone(rooms).lock().unwrap().to_vec())
 }
 
 #[launch]
 // TODO:
 // - Room constructor
 // - Player constructor
-// - Use smart pointer for rooms state
 fn rocket() -> _ {
     rocket::build()
-        .manage(vec![Room {name: String::from("bazinga"), password: String::from("secure")}, Room {name: String::from("one"), password: String::from("gaming")}])
+        .manage(Arc::new(Mutex::new(vec![Room {name: String::from("bazinga"), password: String::from("secure")}, Room {name: String::from("one"), password: String::from("gaming")}])))
         .mount("/", routes![index, rooms])
 }
 
