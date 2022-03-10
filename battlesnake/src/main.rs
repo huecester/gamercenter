f5ea73e095031366487f3b2ac463ae1c8c0af1dd
huecester <jericho.paulino@yahoo.com.ph>
2022-03-10 11:11:29 -0800

    Create rooms

--- battlesnake/src/main.rs
+++ battlesnake/src/main.rs
@@ -1,13 +1,21 @@
 #[macro_use] extern crate rocket;
 
-use std::sync::{Arc, Mutex};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
 use rocket::{
-    serde::json::Json,
+    form::Form,
+    response::status::Created,
+    serde::{
+        json::Json,
+        uuid::Uuid,
+    },
     State,
 };
 use battlesnake_server::types::*;
 
-type Rooms = Arc<Mutex<Vec<Room>>>;
+type Rooms = Arc<Mutex<HashMap<Uuid, Room>>>;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -15,8 +23,24 @@ fn index() -> &'static str {
 }
 
 #[get("/rooms")]
-fn rooms(rooms: &State<Rooms>) -> Json<Vec<Room>> {
-    Json(Arc::clone(rooms).lock().unwrap().to_vec())
+fn list_rooms(rooms: &State<Rooms>) -> Json<Vec<Room>> {
+    let clone = Arc::clone(rooms);
+    let rooms = clone.lock().unwrap();
+
+    Json(rooms.values().map(|room| room.to_owned()).collect())
+}
+
+#[post("/rooms", data = "<room>")]
+fn create_room(room: Form<Room>, rooms: &State<Rooms>) -> Created<String> {
+    let clone = Arc::clone(rooms);
+    let mut rooms = clone.lock().unwrap();
+
+    let room = room.into_inner();
+    let id = Uuid::new_v4();
+    let id_string = id.to_simple().to_string();
+
+    rooms.insert(id, room);
+    Created::new("https://example.com/").body(id_string)
 }
 
 #[launch]
@@ -25,7 +49,7 @@ fn rooms(rooms: &State<Rooms>) -> Json<Vec<Room>> {
 // - Player constructor
 fn rocket() -> _ {
     rocket::build()
-        .manage(Arc::new(Mutex::new(vec![Room {name: String::from("bazinga"), password: String::from("secure")}, Room {name: String::from("one"), password: String::from("gaming")}])))
-        .mount("/", routes![index, rooms])
+        .manage(Arc::new(Mutex::new(HashMap::<Uuid, Room>::new())))
+        .mount("/", routes![index, list_rooms, create_room])
 }
 
--- battlesnake/src/types.rs
+++ battlesnake/src/types.rs
@@ -1,11 +1,33 @@
-use rocket::serde::Serialize;
+use rocket::{
+    form::FromForm,
+    serde::Serialize,
+};
 
-#[derive(Clone, Debug, Serialize)]
+#[derive(Clone, Debug, FromForm, Serialize)]
 pub struct Room {
-    pub name: String,
-    // id: String,
+    #[field(validate = len(1..=32))]
+    name: String,
+
+    #[field(validate = len(..=32))]
+    password: Option<String>,
+
+    // #[field(default = vec![])]
     // players: Vec<Player>,
-    pub password: String,
+}
+
+impl Room {
+    // Methods
+    pub fn get_name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn check_password(&self, input: &str) -> bool {
+        if let Some(password) = &self.password {
+            password == input
+        } else {
+            true
+        }
+    }
 }
 
 
