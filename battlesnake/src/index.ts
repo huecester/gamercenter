82c5c3a8bb908431a850e2e392a769bab37ef576
huecester <jericho.paulino@yahoo.com.ph>
2022-03-31 10:58:02 -0700

    tsconfig update + express Hello, world!

--- battlesnake/src/index.ts
+++ battlesnake/src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello, world!');
+import 'dotenv/config';
+import express from 'express';
+
+const port = process.env.PORT || 8000;
+const app = express();
+
+
+app.get('/', (req, res) => {
+	res.send('Hello, world!');
+});
+
+app.get('/:thing', (req, res) => {
+	res.send(`Hello, ${req.params.thing}!`);
+});
+
+
+app.listen(port, () => {
+	console.log(`App listening on localhost:${port}.`);
+});
