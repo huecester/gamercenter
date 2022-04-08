15faa69633c3a32d71e3b32d39ab5e0f375f750b
huecester <jericho.paulino@yahoo.com.ph>
2022-04-07 17:07:03 -0700

    Player tests

---
+++ battlesnake/src/types/player.spec.ts
@@ -0,0 +1,31 @@
+import { describe } from 'mocha';
+import chai, { expect } from 'chai';
+import { mock } from 'sinon';
+import sinonChai from 'sinon-chai';
+import { faker } from '@faker-js/faker';
+
+import { Player } from './player';
+
+chai.use(sinonChai);
+
+describe('types/players', () => {
+	let username;
+
+	beforeEach(() => {
+		username = faker.internet.userName();
+	});
+
+	it('should be able to be created properly', () => {
+		const player = new Player(username);
+		expect(player).to.be.an('object');
+		expect(player).to.have.a.property('username').that.equals(username);
+		expect(player).to.have.a.property('color').that.matches(/^#[a-f0-9]{6}$/i);
+	});
+
+	it('should be able to be sanitized', () => {
+		const player = new Player(username);
+		const sanitized = player.sanitized();
+
+		expect(sanitized).to.have.all.keys('username', 'color');
+	});
+});
