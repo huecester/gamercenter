f2537987bc02f1cb503b97bedf41e3c754401e48
huecester <jericho.paulino@yahoo.com.ph>
2022-05-19 11:36:56 -0700

    Post rooms

--- battlesnake/src/app.ts
+++ battlesnake/src/app.ts
@@ -1,11 +1,21 @@
 import express from 'express';
 
-import { getSanitizedRooms } from './store/rooms';
+import { Room } from './types/room';
+import { addRoom, getSanitizedRooms } from './store/rooms';
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/rooms', (req, res) => {
 	res.send(getSanitizedRooms());
+});
+
+app.post('/rooms', (req, res) => {
+	const room = new Room(req.body.name);
+	addRoom(room);
+	res.sendStatus(201);
 });
 
 export default app;
