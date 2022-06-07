518ff2e3f19ba12d160b26fdc178ef2c91a1a971
huecester <jericho.paulino@yahoo.com.ph>
2022-06-06 21:09:39 -0700

    Commit

---
+++ battlesnake/test/game.spec.ts
@@ -0,0 +1,10 @@
+import { describe } from 'mocha';
+import { expect, request, use } from 'chai';
+import chaiHttp from 'chai-http';
+import faker from '@faker-js/faker';
+
+use(chaiHttp);
+
+describe('Game', () => {
+	let username;
+});
